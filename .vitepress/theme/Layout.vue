1f91f777a9c96621da72aeaf2053a43749b51663
Erhai-lake <fuzixuan0714_0826@163.com>
2024-10-08 15:16:05 +0800

    站点 新增: 开源协议 脚页

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -3,6 +3,7 @@ import Theme from '@escook/vitepress-theme'
 import ArticleMetadata from '/.vitepress/theme/Components/ArticleMetadata.vue'
 import Contributors from '/.vitepress/theme/Components/Contributors.vue'
 import UpdateLog from '/.vitepress/theme/Components/UpdateLog.vue'
+import Copyright from '/.vitepress/theme/Components/Copyright.vue'
 
 const { Layout } = Theme
 </script>
@@ -15,6 +16,12 @@ const { Layout } = Theme
         <template #doc-footer-before>
             <Contributors />
             <UpdateLog />
+        </template>
+        <template #doc-after>
+            <Copyright />
+        </template>
+        <template #home-features-after>
+            <Copyright />
         </template>
     </Layout>
 </template>
